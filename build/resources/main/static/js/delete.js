45760649fca3420e7fe9514b47d403f83d5b8562
developerKYH <dnfwlq123@naver.com>
2023-10-27 17:44:29 +0900

    hi

--- build/resources/main/static/js/delete.js
+++
@@ -1,8 +0,0 @@
-function deleteBtn(){
-    let $deleteByPid = document.getElementById("pid");
-    let url = "/delete_page/id=" + $deleteByPid.value;
-    fetch(url,{method:"delete"})
-        .then(res=>res.text())
-        .then(res=>$displayZoneContainer.innerHTML=res)
-        .catch(err=>console.err(err));
-}
